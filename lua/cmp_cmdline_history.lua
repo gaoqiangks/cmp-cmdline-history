7f83422a7fe423a4b1edf0c9e324af52efba2038
gaoqiang <gaoqiangks@qq.com>
2025-04-22 23:42:46 +0800

    a

--- lua/cmp_cmdline_history.lua
+++ lua/cmp_cmdline_history.lua
@@ -1,25 +1,28 @@
 local source = {}
 
 function source.new()
-  return setmetatable({}, { __index = source })
+    return setmetatable({}, { __index = source })
 end
 
 function source:get_keyword_pattern()
-  return '.*'
+    return '.*'
 end
 
 function source:complete(request, callback)
-  local hist_type = request.option.history_type or vim.fn.getcmdtype()
-  local seen_items = {}
-  local items = {}
-  for i = 1, vim.fn.histnr(hist_type) do
-    local item = vim.fn.histget(hist_type, -i)
-    if #item > 0 and not seen_items[item] then
-      seen_items[item] = true
-      items[#items + 1] = { label = item, dup = 0 }
+    local hist_type = request.option.history_type or vim.fn.getcmdtype()
+    local seen_items = {}
+    local items = {}
+    local index = 1
+    for i = 1, vim.fn.histnr(hist_type) do
+        local item = vim.fn.histget(hist_type, -i)
+        if #item > 0 and not seen_items[item] then
+            seen_items[item] = true
+            items[#items + 1] = { label = item, dup = 0, histnr = index }
+            index = index + 1
+        end
     end
-  end
-  callback({ items = items })
+    log("items = " .. vim.inspect(items))
+    callback({ items = items })
 end
 
 return source
